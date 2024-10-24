a80c9bb7eab1bfe4ba74687ee7cd254d419ceed6
Amanda Marcos <amandakkm0202@gmail.com>
2024-10-24 16:49:55 -0300

    Pastas

--- prova-front/src/App.tsx
+++ prova-front/src/App.tsx
@@ -33,3 +33,21 @@ function App() {
 }
 
 export default App
+
+// import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+// import Home from './Paginas/Home/Home'
+// import './App.css'
+// import CadastroUsuario from "./Paginas/CadastroUsuario/CadastroUsuario";
+
+// const App = () => {
+//   return(
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/CadastroUsuarios" element={<CadastroUsuario/>}/>
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App
